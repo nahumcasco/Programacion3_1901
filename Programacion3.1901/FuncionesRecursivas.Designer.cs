909fc610403ff2f9c55d60fd5861067f751c2af1
Nahum Casco <40469753+nahumcasco@users.noreply.github.com>
2021-09-21 19:51:57 -0600

    Clase 21 Septiembre

---
+++ Programacion3.1901/FuncionesRecursivas.Designer.cs
@@ -0,0 +1,123 @@
+﻿
+namespace Programacion3._1901
+{
+    partial class FuncionesRecursivas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.ResultadoFactoriallabel = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.NumerotextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(238, 33);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ResultadoFactoriallabel
+            // 
+            this.ResultadoFactoriallabel.AutoSize = true;
+            this.ResultadoFactoriallabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ResultadoFactoriallabel.ForeColor = System.Drawing.Color.YellowGreen;
+            this.ResultadoFactoriallabel.Location = new System.Drawing.Point(169, 301);
+            this.ResultadoFactoriallabel.Name = "ResultadoFactoriallabel";
+            this.ResultadoFactoriallabel.Size = new System.Drawing.Size(66, 24);
+            this.ResultadoFactoriallabel.TabIndex = 1;
+            this.ResultadoFactoriallabel.Text = "label1";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(404, 33);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(110, 147);
+            this.listBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(571, 33);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(160, 203);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Factorial";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // NumerotextBox
+            // 
+            this.NumerotextBox.Location = new System.Drawing.Point(160, 244);
+            this.NumerotextBox.Name = "NumerotextBox";
+            this.NumerotextBox.Size = new System.Drawing.Size(75, 20);
+            this.NumerotextBox.TabIndex = 5;
+            // 
+            // FuncionesRecursivas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(788, 410);
+            this.Controls.Add(this.NumerotextBox);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.ResultadoFactoriallabel);
+            this.Controls.Add(this.button1);
+            this.Name = "FuncionesRecursivas";
+            this.Text = "FuncionesRecursivas";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label ResultadoFactoriallabel;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox NumerotextBox;
+    }
+}
---
+++ Programacion3.1901/FuncionesRecursivas.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Programacion3._1901
+{
+    public partial class FuncionesRecursivas : Form
+    {
+        public FuncionesRecursivas()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            RecorrerDeUnoEnUno(1, 20);
+        }
+
+        private void RecorrerDeUnoEnUno(int num1, int num2)
+        {
+            listBox1.Items.Add(num1);
+            if (num1 < num2)
+            {
+                RecorrerDeUnoEnUno(num1 + 1, num2);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (int i = 1; i <= 20; i++)
+            {
+                listBox1.Items.Add(i);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ResultadoFactoriallabel.Text = FactorialDe(Convert.ToDecimal(NumerotextBox.Text)).ToString();
+        }
+
+        private decimal FactorialDe(decimal numero)
+        {
+            if (numero == 1)
+            {
+                return numero;
+            }
+            else
+            {
+                return numero * FactorialDe(numero - 1);
+            }
+        }
+
+    }
+}
